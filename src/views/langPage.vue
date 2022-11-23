ef5005c64e3ba14da91466622d0c7b83ecf03a6d
t871631092 <971340511@qq.com>
2022-11-23 20:05:22 +0800

    add bug

---
+++ src/views/langPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <ion-page>
+    <ion-router-outlet />
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { IonPage, IonRouterOutlet } from '@ionic/vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'langPage',
+  components: {
+    IonPage,
+    IonRouterOutlet
+  }
+});
+</script>
